fdb6fed67a5aa551370f6476885dc2654e814b8f
Gavin Chou <gavineaglechou@gmail.com>
2019-06-12 10:18:06 +0800

    Modify anchor of TOC elements only

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,29 +98,33 @@ var sharing = function(){
 
 // jumping with offset
 $(document).ready(function() {
-  // replace all href click behavior
-  // https://api.jquery.com/category/selectors/
-  // TODO: process anchors only, skip unnecessary hrefs
-  $('a[href^="#"]:not([href="#menu"]').click(function() {
+  var scroll_time_ms = 600;
+  var top_offset = 80;
+  var foo = function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
         && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 125 //offsets for fixed header
-        }, 1000);
+          scrollTop: target.offset().top - top_offset //offsets for fixed header
+        }, scroll_time_ms);
         return false;
       }
     }
-  });
+  };
+  // modifiy all href click behavior
+  // https://api.jquery.com/category/selectors/
+  // TODO: add markdown generated anchor ref
+  $("#markdown-toc").find('a[href^="#"]').click(foo);
+  $("#__table-of-contents").find('a[href^="#"]').click(foo);
   // Executed on page load with URL containing an anchor tag.
   if($(location.href.split("#")[1])) {
     var target = $('#'+location.href.split("#")[1]);
     if (target.length) {
       $('html,body').animate({
-        scrollTop: target.offset().top - 125 //offset height of header here too.
-      }, 1000);
+          scrollTop: target.offset().top - top_offset //offsets for fixed header
+      }, scroll_time_ms);
       return false;
     }
   }
